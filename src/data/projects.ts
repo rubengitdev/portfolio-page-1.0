65e428570ab9841ada4e1ea0cc46a474b247cd36
rubengitdev <rubencahyadi504@gmail.com>
2026-08-14 06:06:38 +0700

    data: add dummy projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,32 @@
+import type { Project } from '../types';
+
+export const projects: Project[] = [
+    {
+        id: 1,
+        title: 'title-1',
+        description: 'description-1',
+        category: 'closed-source',
+        technologies: ['React', 'TypeScript', 'Node.js'],
+    },
+    {
+        id: 2,
+        title: 'title-2',
+        description: 'description-2',
+        category: 'open-source',
+        technologies: ['React', 'TypeScript', 'Node.js'],
+    },
+    {
+        id: 3,
+        title: 'title-3',
+        description: 'description-3',
+        category: 'open-source',
+        technologies: ['React', 'TypeScript', 'Node.js'],
+    },
+    {
+        id: 4,
+        title: 'title-4',
+        description: 'description-4',
+        category: 'closed-source',
+        technologies: ['React', 'TypeScript', 'Node.js'],
+    },
+];
